12f1e8335e8c7f8d3eb80c69816642b6c737a6f0
Leo Allen <leo.f.allen@gmail.com>
2012-04-13 00:00:59 +0100

    Got a shell script working, but symbolic links seem to not work with it for some reason

--- enhance-propane.sh
+++ enhance-propane.sh
@@ -1,2 +1,2 @@
-# ~/Library/Application Support/Propane/styles/cf_chat.css
-~/Library/Application Support/Propane/unsupported/caveatPatchor.js
+cp cf_chat.css ~/Library/Application\ Support/Propane/styles/cf_chat.css
+cp caveatPatchor.js ~/Library/Application\ Support/Propane/unsupported/caveatPatchor.js
