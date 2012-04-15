ce6f27048a05d72fcfecde0b6c38f012db568def
Michael Strassburger <michael@couchsurfing.org>
2012-04-15 16:35:22 +0200

    Updating file structure, merging files together on enhance-propane.sh call, adding syntax highlighter framework

--- enhance-propane.sh
+++ enhance-propane.sh
@@ -1,2 +1,2 @@
-cp cf_chat.css ~/Library/Application\ Support/Propane/styles/cf_chat.css
-cp caveatPatchor.js ~/Library/Application\ Support/Propane/unsupported/caveatPatchor.js
+cat cf_chat.css syntaxhighlighter.css custom.css >~/Library/Application\ Support/Propane/styles/cf_chat.css
+cat syntaxhighlighter.js custom.js >~/Library/Application\ Support/Propane/unsupported/caveatPatchor.js
