5f83e70e441a773519ddf246c067bd6cd411f915
Michael Straßburger <m.strassburger@gmail.com>
2012-04-15 17:37:23 +0300

    Fixing path to original cf_chat.css file

--- enhance-propane.sh
+++ enhance-propane.sh
@@ -1,2 +1,2 @@
-cat cf_chat.css syntaxhighlighter.css custom.css >~/Library/Application\ Support/Propane/styles/cf_chat.css
+cat original/cf_chat.css syntaxhighlighter.css custom.css >~/Library/Application\ Support/Propane/styles/cf_chat.css
 cat syntaxhighlighter.js custom.js >~/Library/Application\ Support/Propane/unsupported/caveatPatchor.js
